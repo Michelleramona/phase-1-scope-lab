7b634d987012d7d58969da77b31d7a049dc9da1b
Michelle Vitello <michelleramona89@gmail.com>
2022-09-11 17:27:05 -0400

    commit tree eafe6ff8f272dea0ed4e479aa938750c937cbcad

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    return customerName=customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer = 'maybe bob'
+}
+
+const LeastFavoriteCustomer = "jim"
+
+function changeLeastFavoriteCustomer(){
+    return LeastFavoriteCustomer = "mish"
+}
